20694964f91dc105bc260ad0625aa14057cf8b5f
Gustavo <ghgennari@gmail.com>
2025-04-01 11:13:57 -0300

    first commit
    adicionando no github. codigo ainda com erros

---
+++ src/app/dados.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Student } from './student';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DadosService {
+  apiUrl = "http://localhost:3000/students"
+
+  constructor(private http: HttpClient) {}
+
+  getStudents(): Observable<Student[]>{
+    return this.http.get<Student[]>(this.apiUrl);
+  }
+}
---
+++ src/app/students/students.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { DadosService } from '../dados.service';
+
+@Component({
+  selector: 'app-students',
+  standalone: false,
+  templateUrl: './students.component.html',
+  styleUrl: './students.component.css'
+})
+export class StudentsComponent implements OnInit {
+
+  students: Student[] = [];
+  
+  constructor(private service: DadosService){}
+
+  ngOnInit(): void {
+      this.service.getStudents().subscribe({
+        next: json => this.students = json
+      })
+  }
+
+
+
+
+  
+
+}
