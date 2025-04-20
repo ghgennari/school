e58140e386119cc5cb61c33f31814af253954557
Gustavo <ghgennari@gmail.com>
2025-04-20 16:14:39 -0300

    course 0.2

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { DadosService } from '../dados.service';
+import { Course } from '../course';
 
 @Component({
   selector: 'app-courses',
@@ -8,4 +11,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CoursesComponent implements OnInit {
 
+  courses: Course[] = [];
+  formGroupCourse: FormGroup;
+
+  constructor(private service: DadosService,
+              private formBuilder: FormBuilder
+  ){
+    this.formGroupCourse = formBuilder.group({
+      id: [''],
+      name: [''],
+      price: [''],
+      active: [false],
+      promotion: [false],
+    });
+  }
 }
